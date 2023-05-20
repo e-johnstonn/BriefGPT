fb21d480bd3bcd3de84e26e3554737d3343d1602
HardSoft2023 <909104374@qq.com>
2023-05-20 19:54:27 +0800

    Revert "embedding with text2vec-large-chinese"
    This reverts commit f320e5e975cd622396571d942fc6639803f00417.

--- local_chat_utils.py
+++ local_chat_utils.py
@@ -1,21 +1,17 @@
 import os
 
 from langchain import FAISS
-from langchain.embeddings import HuggingFaceInstructEmbeddings, HuggingFaceEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from summary_utils import doc_loader, remove_special_tokens
 
 import streamlit as st
-def get_embeddings(docs_lang):
-    if docs_lang.upper() in ['CHINESE', 'ZH']:
-        return HuggingFaceEmbeddings(model_name=os.getenv('EMBED_PATH'))
-    else:
-        return HuggingFaceInstructEmbeddings(model_name='hkunlp/instructor-base')
 
-def create_and_save_local(file_path, docs_lang):
+
+def create_and_save_local(file_path, model_name):
     name = os.path.split(file_path)[1].split('.')[0]
-    embeddings = get_embeddings(docs_lang)
+    embeddings = HuggingFaceInstructEmbeddings(model_name=model_name)
     doc = doc_loader(file_path)
     splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     split_docs = splitter.split_documents(doc)
@@ -24,23 +20,23 @@ def create_and_save_local(file_path, docs_lang):
     db.save_local(folder_path='local_embeddings', index_name=name)
 
 
-def load_local_embeddings(file_path, docs_lang):
+def load_local_embeddings(file_path, model_name):
     name = os.path.split(file_path)[1].split('.')[0]
-    embeddings = get_embeddings(docs_lang)
+    embeddings = HuggingFaceInstructEmbeddings(model_name=model_name)
     db = FAISS.load_local(folder_path='local_embeddings', index_name=name, embeddings=embeddings)
     return db
 
 
 
-def load_db_from_file_and_create_if_not_exists_local(file_path, docs_lang):
+def load_db_from_file_and_create_if_not_exists_local(file_path, model_name):
     with st.spinner('Loading chat embeddings...'):
         try:
-            db = load_local_embeddings(file_path, docs_lang)
+            db = load_local_embeddings(file_path, model_name)
             print('success')
         except RuntimeError:
             print('not found')
-            create_and_save_local(file_path, docs_lang)
-            db = load_local_embeddings(file_path, docs_lang)
+            create_and_save_local(file_path, model_name)
+            db = load_local_embeddings(file_path, model_name)
     if db:
         st.success('Loaded successfully! Start a chat below.')
     else:
